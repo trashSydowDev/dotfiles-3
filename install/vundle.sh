d8081efe0d53e8fe17dfaa22ca8ffb9e6124f6e6
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-09-28 14:22:59 -0300

    fix(vim): theme copy

--- install/vundle.sh
+++ install/vundle.sh
@@ -4,7 +4,7 @@ BUNDLE_DIR=~/.vim/bundle
 mkdir -p "$BUNDLE_DIR" && (git clone https://github.com/gmarik/vundle.git "$BUNDLE_DIR/vundle" || (cd "$BUNDLE_DIR/vundle" && git pull origin master))
 
 # Copy themes
-cp -r $DOTFILES_DIR/vim/themes/ ~/.vim/colors/
+cp -r $DOTFILES_DIR/vim/themes/**/colors/*.vim ~/.vim/colors/
 
 # Install bundles
 vim +PluginInstall +qall

1921df20b3fddb237c0129193574d3f4c97ad47b
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-09-29 20:40:54 -0300

    feature(dock): adding downloads dir

--- osx/dock.sh
+++ osx/dock.sh
@@ -6,5 +6,6 @@ dockutil --no-restart --add "/Applications/Google Chrome.app/"
 dockutil --no-restart --add "/Applications/Reeder.app/"
 dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "$HOME/Applications/Spotify.app"
+dockutil --no-restart --add "~/Downloads/"
 
 killall Dock
